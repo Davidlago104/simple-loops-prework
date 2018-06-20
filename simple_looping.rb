f02701008082971ebe394d88224c2f9e25ba9df5
Davidlago104 <davidlago104@gmail.com>
2018-06-20 23:03:43 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,6 +38,8 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
   while counter < 7 
+  counter += 1 
+  puts phrase
   
   
 end
